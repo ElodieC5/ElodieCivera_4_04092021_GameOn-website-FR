10e40f372c04fc9923aee3abf014c471514e202a
Elodie Civera <elodie.civera5@gmail.com>
2021-09-04 17:32:41 +0200

    Initial commit with modal closing done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// NAV Responsive with icon for screen & max-width 768px
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,9 +9,11 @@ function editNav() {
 }
 
 // DOM Elements
+// modalbg: bckg form, modalBtn: btn to launch, formData: inputs, span: (x) to close
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const span = document.getElementsByClassName("close")[0];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +21,18 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalbg.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalbg) {
+    modalbg.style.display = "none";
+  }
 }
 
 
